36516d50b7ff130e5133478affd8b8bf40831dc0
Damien <damien@guillot.nu>
2018-05-20 09:35:52 +0200

    Forgot button.ts

--- src/Sensors/Button.ts
+++ src/Sensors/Button.ts
@@ -2,11 +2,19 @@ import GenericSensor from "./GenericSensor";
 import HubMessage from "../Types/HubMessage";
 export default class Button extends GenericSensor {
 
+    private battery: number = null;
+
     onMessage(message: HubMessage)
     {
+        if (message.data.voltage)
+        {
+            this.battery = (parseInt(message.data.voltage) - this.minVolt) / (this.maxVolt - this.minVolt);
+            this.battery = Math.round(this.battery * 100);
+        }
+        
         // could be click, double_click, long_click_press, long_click_release - see hub::clickTypes
         if (message.cmd == 'report') {
-            this.hub.emit('data.button', this.sid, message.data.status);
+            this.hub.emit('data.button', this.sid, message.data.status, this.battery);
         }
     }
 
